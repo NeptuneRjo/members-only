213331d2d1a802f5519489e6b05726aeea50bffb
NeptuneRjo <rjo9000@outlook.com>
2022-06-09 14:44:54 -0400

    create user function built using bcrypt password hashing

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
+const createUser = async (req, res, next) => {
+	bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+		// error catch for hash
+		if (err) {
+			return next(err)
+		}
+
+		const user = new User({
+			username: req.body.username,
+			password: hashedPassword,
+		}).save((err) => {
+			// error catch for new user
+			if (err) {
+				return next(err)
+			}
+
+			res.redirect('/')
+		})
+	})
+}
