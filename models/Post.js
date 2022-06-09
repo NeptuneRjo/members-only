6a7bb728678e62b6601cb22d4f65eca108c9730b
NeptuneRjo <rjo9000@outlook.com>
2022-06-09 13:01:32 -0400

    created model for posts

---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: [true, 'please provide a title'],
+		trim: true,
+	},
+	body: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: String,
+	},
+	dateCreated: {
+		Type: String,
+	},
+})
