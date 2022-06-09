11ec37715936704a46f1321ed98a04e8c4a189b3
NeptuneRjo <rjo9000@outlook.com>
2022-06-09 02:42:05 -0400

    imports; setup db, express and passport

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const User = require('./models/User')
+
+const Schema = mongoose.Schema
+require('dotenv').config()
+
+// Database
+const mongoURI = process.env.MONGO_URI
+mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'mongo connection error'))
+
+// express
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// Passport
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
+
+passport.serializeUser((id, done) => {
+	User.findById(id, (err, user) => {
+		done(err, user)
+	})
+})
+
+passport.deserializeUser((id, done) => {
+	User.findById(id, (err, user) => {
+		done(err, user)
+	})
+})
+
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		User.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err)
+			}
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' })
+			}
+			if (user.password !== password) {
+				bcrypt.compare(password, user.password, (err, res) => {
+					if (res) {
+						return done(null, user)
+					} else {
+						return done(null, false, { message: 'Incorrect password' })
+					}
+				})
+			}
+		})
+	})
+)
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user
+	next()
+})
