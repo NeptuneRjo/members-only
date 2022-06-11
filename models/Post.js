81a04aa774fcee26271bba5e09ab143316e59b84
NeptuneRjo <rjo9000@outlook.com>
2022-06-10 20:51:55 -0400

    added timestamps

--- models/Post.js
+++ models/Post.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
 
-const PostSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: [true, 'please provide a title'],
-		trim: true,
+const PostSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: [true, 'please provide a title'],
+			trim: true,
+		},
+		body: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: String,
+		},
 	},
-	body: {
-		type: String,
-		required: true,
-	},
-	author: {
-		type: String,
-	},
-	dateCreated: {
-		Type: String,
-	},
-})
+	{ timestamps: true }
+)
 
 module.exports = mongoose.model('Post', PostSchema)
