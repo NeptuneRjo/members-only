0b60a297935be6a7a96338f68ab6660233679eb1
NeptuneRjo <rjo9000@outlook.com>
2022-06-15 18:27:36 -0400

    added GET and POST routes for create-post

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,15 @@ const router = express.Router()
 
 const {
 	getAllPosts,
+	getCreatePost,
 	createPost,
 	getPost,
 	deletePost,
 } = require('../controllers/post')
 
-router.route('/').get(getAllPosts).post(createPost)
+router.route('/').get(getAllPosts)
+router.route('/create-post').get(getCreatePost).post(createPost)
+
 router.route('/:id').get(getPost).delete(deletePost)
 
 module.exports = router
