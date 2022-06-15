57699e97a788aa8227ff932751c3eb8984c80b26
NeptuneRjo <rjo9000@outlook.com>
2022-06-15 14:41:28 -0400

    implemented additional routes for the sign in/up/out pages

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const { createUser } = require('../controllers/user')
+const { createUser, signInUser, signOutUser } = require('../controllers/user')
 
-router.post('/', createUser)
+router
+	.route('/sign-up')
+	.post(createUser)
+	.get((req, res) => res.render('sign-up', { title: 'Sign Up' }))
+
+router
+	.route('/sign-in')
+	.post(signInUser)
+	.get((req, res) => res.render('sign-in', { title: 'Sign In' }))
+
+router.route('/sign-out').get(signOutUser)
 
 module.exports = router
