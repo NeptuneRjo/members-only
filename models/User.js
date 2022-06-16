c694045ffb331a21e3b48525d08f5ccf84c0738c
NeptuneRjo <rjo9000@outlook.com>
2022-06-16 17:25:10 -0400

    removed isAdmin param

--- models/User.js
+++ models/User.js
@@ -10,10 +10,6 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'must enter a password'],
 	},
-	isAdmin: {
-		type: Boolean,
-		required: true,
-	},
 })
 
 module.exports = mongoose.model('User', UserSchema)
